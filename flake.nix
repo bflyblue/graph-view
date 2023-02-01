f4a11164d1ec8e2b4dcf7dc15e1f0fc8141977a5
Shaun Sharples <shaun.sharples@gmail.com>
2023-02-01 10:34:14 +0200

    initial

--- flake.nix
+++ flake.nix
@@ -1,10 +1,37 @@
 {
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
-  outputs = inp:
-    inp.dream2nix.lib.makeFlakeOutputs {
-      systemsFromFile = ./nix_systems;
-      config.projectRoot = ./.;
-      source = ./.;
-      projects = ./projects.toml;
+  description = "graph-view";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
+    graph-view-web = { url = "./web"; };
+    graph-view-server = { url = "./server"; };
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils, graph-view-web, graph-view-server, ... }:
+    let
+      # pkgs = import nixpkgs { system = "x86_64-linux"; };
+      supportedSystems = [ "x86_64-linux" ];
+      forallSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forallSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forallSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          graph-view-server = graph-view-server.packages.${system}.default;
+          graph-view-web = graph-view-web.packages.${system}.default;
+          graph-view = pkgs.writeShellScriptBin "graph-view" ''
+            ${self.packages.${system}.graph-view-server}/graph-view-server ${
+              self.packages.${system}.graph-view-web
+            }
+          '';
+          default = self.packages.${system}.graph-view;
+        });
+    };
+
 }
---
+++ server/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "graph-view-server";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # hlib = pkgs.haskell.lib;
+        haskellPackages = pkgs.haskellPackages.override {
+          overrides = self: super: { };
+
+        };
+        hls = pkgs.haskell-language-server.override { dynamic = true; };
+        packageName = "graph-view-server";
+      in {
+        packages.${packageName} =
+          haskellPackages.callCabal2nix packageName self { };
+
+        packages.default = self.packages.${system}.${packageName};
+
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [ self.packages.${system}.${packageName} ];
+          buildInputs = [ haskellPackages.cabal-install hls ];
+          withHoogle = true;
+        };
+      });
+}
