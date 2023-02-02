9eb563f26738860883429bc9239ec7ad980ac264
Shaun Sharples <shaun.sharples@gmail.com>
2023-02-02 15:01:59 +0200

    wip

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -1,11 +1,12 @@
-import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import html from "@rollup/plugin-html";
 
 export default defineConfig({
   input: "src/main.ts",
   output: {
-    file: "dist/bundle.js",
+    dir: "dist",
     format: "iife",
   },
-  plugins: [typescript()],
+  plugins: [typescript(), html()],
 });
